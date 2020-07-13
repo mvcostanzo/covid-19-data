c91fe4d70ceaa09aa8b7146a693709b3905d7832
Michael Costanzo <mvcostanzo@outlook.com>
2020-07-13 16:37:46 -0400

    Adding Batch Script

--- Build Scripts/build.bat
+++ Build Scripts/build.bat
@@ -1,9 +1,11 @@
-cd "C:\Michael_Docs\Git Repositories\NY Times COVID Data"
+cd "C:\Michael_Docs\Git Repositories\NY Times COVID Data\NYC COVID Details"
 git fetch --progress --verbose upstream
 git merge upstream/master
 git push --progress --verbose origin refs/heads/master:refs/heads/master --tags
 
-cd "C:\Michael_Docs\Git Repositories\NY Times COVID Data\NYC COVID Details"
+cd "C:\Michael_Docs\Git Repositories\NY Times COVID Data"
+git submodule update --init --recursive -- "C:\Michael_Docs\Git Repositories\NY Times COVID Data\NYC COVID Details"
 git fetch --progress --verbose upstream
 git merge upstream/master
+git commit -a --file="C:/Michael_Docs/Git Repositories/NY Times COVID Data/.git/COMMITMESSAGE"
 git push --progress --verbose origin refs/heads/master:refs/heads/master --tags
