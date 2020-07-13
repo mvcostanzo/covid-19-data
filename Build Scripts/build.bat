13ebbc9be8a4c0d27899f4ffc114c04b6e5ea3e4
Michael Costanzo <mvcostanzo@outlook.com>
2020-07-13 17:15:07 -0400

    Adding Batch Script

--- Build Scripts/build.bat
+++ Build Scripts/build.bat
@@ -3,6 +3,11 @@ git fetch --progress --verbose upstream
 git merge upstream/master
 git push --progress --verbose origin refs/heads/master:refs/heads/master --tags
 
+
+cd "C:\Michael_Docs\Git Repositories\NY Times COVID Data\Python"
+python NYCCleanup.py
+
+
 cd "C:\Michael_Docs\Git Repositories\NY Times COVID Data"
 git submodule update --init --recursive -- "C:\Michael_Docs\Git Repositories\NY Times COVID Data\NYC COVID Details"
 git fetch --progress --verbose upstream
