6323bb1baef2f372daa77ddc2911cd4bd9a5a9e2
Michael Costanzo <mvcostanzo@outlook.com>
2020-07-20 08:35:27 -0400

    Update to NY COVID Data

--- Build Scripts/build.bat
+++ Build Scripts/build.bat
@@ -1,15 +1,16 @@
 cd "C:\Michael_Docs\Git Repositories\NY Times COVID Data\NYC COVID Details"
+git checkout master
 git fetch --progress --verbose upstream
 git merge upstream/master
 git push --progress --verbose origin refs/heads/master:refs/heads/master --tags
-
+git checkout master
 
 cd "C:\Michael_Docs\Git Repositories\NY Times COVID Data\Python"
 python NYCCleanup.py
 
 
 cd "C:\Michael_Docs\Git Repositories\NY Times COVID Data"
-git submodule update --init --recursive -- "C:\Michael_Docs\Git Repositories\NY Times COVID Data\NYC COVID Details"
+git add "C:\Michael_Docs\Git Repositories\NY Times COVID Data\NYC COVID Details"
 git fetch --progress --verbose upstream
 git merge upstream/master
 git commit -a --file="C:/Michael_Docs/Git Repositories/NY Times COVID Data/.git/COMMITMESSAGE"
